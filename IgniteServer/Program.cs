083fcb87c0a6a4b2c07e22c3a9409e6f26cc0ec7
Bilal Mateen <bilalmateen825@outlook.com>
2024-09-12 00:02:24 +0500

    ignite-changes

--- IgniteServer/Program.cs
+++ IgniteServer/Program.cs
@@ -25,22 +25,36 @@ public class Program
                     Endpoints = new[] { "127.0.0.1:47500..47509" }
                 }
             },
-           
-            CommunicationSpi = new TcpCommunicationSpi { IdleConnectionTimeout = TimeSpan.FromSeconds(5) }
+
+            CommunicationSpi = new TcpCommunicationSpi { IdleConnectionTimeout = TimeSpan.FromSeconds(5) },
+
+            //https://ignite.apache.org/docs/latest/persistence/native-persistence
+            DataStorageConfiguration = new Apache.Ignite.Core.Configuration.DataStorageConfiguration()
+            {
+                StoragePath = "/ssd/storage",
+                DefaultDataRegionConfiguration = new Apache.Ignite.Core.Configuration.DataRegionConfiguration()
+                {
+                    Name = "Default_Region",
+                    PersistenceEnabled = true,
+
+                }
+            }
         };
 
         using (var ignite = Ignition.Start(cfg))
         {
+            ignite.GetCluster().DisableWal(CacheName);
             var cache = ignite.GetOrCreateCache<int, Person>(new CacheConfiguration
             {
                 Name = CacheName,
                 QueryEntities = new[] { new QueryEntity(typeof(int), typeof(Person)) }
             });
 
+
             var qry = new ContinuousQuery<int, Person>(new LocalListener(), new RemoteFilter());
 
             cache.QueryContinuous(qry);
-
+            ignite.GetCluster().EnableWal(CacheName);
             Console.WriteLine("Server started. Press any key to exit...");
             Console.ReadKey();
         }
@@ -53,7 +67,8 @@ public class Program
             foreach (var evt in events)
             {
                 Console.WriteLine($"Intercepted request from client: {evt.EventType} [Key={evt.Key}, Value={evt.Value}]");
-
+                var cache = Ignition.GetIgnite().GetOrCreateCache<int, Person>(CacheName);
+                cache[evt.Key] = evt.Value;
                 // Perform some operations here before the data is added to the cache and database
             }
         }
